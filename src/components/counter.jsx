7a5ef5ccd26bbf84a2cd83fa3daa7f721dfe652a
amirtabatabaii <tabatabaii.amir@outlook.comm>
2020-01-02 12:32:12 +0300

    add route

---
+++ src/components/counter.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+      // Ajax call and get new data from the server
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Counter - Unmount");
+  }
+
+  render() {
+    console.log("Counter - Rendered");
+
+    return (
+      <React.Fragment>
+        <div className="">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
+          <button
+            className="btn btn-danger btn-sm m-2"
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+          >
+            Delete
+          </button>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-3 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.props.counter;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
