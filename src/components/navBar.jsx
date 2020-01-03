f01aec92b20e57d550792badac4ed3b01428d360
amirtabatabaii <tabatabaii.amir@outlook.comm>
2020-01-03 12:50:43 +0300

    add registerForm

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -29,6 +29,9 @@ const NavBar = () => {
           <NavLink className="nav-item nav-link" to="/rentals">
             rentals
           </NavLink>
+          <NavLink className="nav-item nav-link" to="/register">
+            Register
+          </NavLink>
           <NavLink className="nav-item nav-link" to="/login">
             Login
           </NavLink>
